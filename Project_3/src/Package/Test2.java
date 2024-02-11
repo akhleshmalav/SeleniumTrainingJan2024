9c8f3bb6acd29c8ae76c10d9dca147cfad6ec5fa
Administrator <Administrator@DESKTOP-J8KTD2I>
2024-02-11 13:06:06 +0530

    Third commit

---
+++ Project_3/src/Package/Test2.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("This is first program");
+		System.out.println("This is first program");
+		System.out.println("This is first program");
+		System.out.println("This is first program");
+		
+	}
+}
